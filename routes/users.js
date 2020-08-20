a49f5c24d4b9ca2dcba0b984a8fba71c4ab67516
philc <32604473+Phil160@users.noreply.github.com>
2020-08-21 08:43:55 +1200

    add views to match routes

--- routes/users.js
+++ routes/users.js
@@ -11,29 +11,29 @@ router.get('/', (req, res) => {
   res.render('home', viewData)
 })
 
-//display page for images
-router.get('/image/:id', (req, res) => {
-
-})
-
-//posting up a new comment to images page
-router.post('/image/:id', (req, res) => {
-  
+// stretch content - a gallery to display a grid of images as thumbnails
+router.get('/gallery', (req, res) => {
+  res.render('gallery', {})
 })
 
 //display form for adding new images
-router.get('/image/new', (req, res) => {
-  
+router.get('/new', (req, res) => {
+  res.render('newImage', {})
 })
 
 //posting up a new image
-router.post('/image/new', (req, res) => {
-  
+router.post('/new', (req, res) => {
+  res.redirect('image', {}) //should redirect to image page, pass through the id to display new image
 })
 
-// stretch content - a gallery to display a grid of images as thumbnails
-router.get('/gallery', (req, res) => {
+//display page for imagesgit a
+router.get('/:id', (req, res) => {
+  res.render('image', {})
+})
 
+//posting up a new comment to images page
+router.post('/:id', (req, res) => {
+  res.redirect(req.get('referer')) //redirects to same page you were just on
 })
 
 module.exports = router
