9cfc8c4d49330e94e29bb535ba22235ac320d122
philc <32604473+Phil160@users.noreply.github.com>
2020-08-21 09:31:01 +1200

    update route and view template

--- routes/users.js
+++ routes/users.js
@@ -25,28 +25,23 @@ router.get('/', (req, res) => {
   res.render('home', viewData)
 })
 
-// stretch content - a gallery to display a grid of images as thumbnails
-router.get('/gallery', (req, res) => {
-  res.render('gallery', {})
-})
+// // stretch content - a gallery to display a grid of images as thumbnails
+// router.get('/gallery', (req, res) => {
+//   res.render('gallery', {})
+// })
 
-//display form for adding new images
-router.get('/new', (req, res) => {
-  res.render('newImage', {})
-})
-
-//posting up a new image
-router.post('/new', (req, res) => {
-  res.redirect('image', {}) //should redirect to image page, pass through the id to display new image
-})
-
-//display page for imagesgit a
+//display page for images
 router.get('/:id', (req, res) => {
   res.render('image', {})
 })
 
+//posting up a new image to images page
+router.post('/newImage', (req, res) => {
+  res.redirect(req.get('referer')) //redirects to same page you were just on
+})
+
 //posting up a new comment to images page
-router.post('/:id', (req, res) => {
+router.post('/newComment', (req, res) => {
   res.redirect(req.get('referer')) //redirects to same page you were just on
 })
 
